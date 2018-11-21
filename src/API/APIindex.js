92c2b8d56af31a890994e92cf24fa18238b4956a
Christopher Salinas <salinas23cfh@gmail.com>
2018-11-20 20:09:00 -0600

    adding foursqaure API

---
+++ src/API/APIindex.js
@@ -0,0 +1,51 @@
+class Helper {
+    static baseURL() {
+        return "https://api.foursquare.com/v2";
+    }
+    static auth() {
+        const keys = {
+            client_id: "SAIXBZ1XZCBXC45ELYBMOPOXXLQ2ZXCMBHGYVVE53HFQFHI4",
+            client_secret: "0MVFYMZDBKCPCVYTFJJP5YIMS4TPVOVPLSUFI3AMWEEFVQGN",
+            v: "20181120"
+        };
+        return Object.keys(keys).map(key => `${key}=${keys[key]}`).join("&");
+    }
+    static urlBuilder(urlPrams) {
+        if(!urlPrams) {
+            return '';
+        }
+        return Object.keys(urlPrams).map(key => `${key}=${urlPrams[key]}`).join("&");
+    }
+    static headers() {
+        return {
+            Accept: "application/json"
+        };
+        
+    }
+    static simpleFetch(endpoints, method, urlPrams) {
+        let requestData = {
+            method,
+            headers: Helper.headers()
+        };
+        return fetch(`${Helper.baseURL()}${endpoints}?${Helper.auth()}&${Helper.urlBuilder(urlPrams)}`, requestData)
+        .then(res => res.json());
+
+    }
+}
+
+export default class SquareAPI {
+    static search(urlPrams) {
+        return Helper.simpleFetch("/venues/VENUE_ID", "GET", urlPrams);
+    }
+    static getVenuesDetails(VENUE_ID) {
+        return Helper.simpleFetch(`/venues/${VENUE_ID}`, "GET");
+    }
+    static getVenuesPhotos(VENUE_ID) {
+        return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`, "GET");
+    }
+}
+
+
+
+
+
--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Maps from './component/Maps';
+import SquareAPI from "./API/APIindex"
 
 class App extends Component {
+  componentDidMount() {
+    SquareAPI.search({
+      near: "Austin,TX",
+      query: "tacos",
+      limit: 10
+    }).then(results => console.log(results));
+  }
   render() {
     return (
       <div className="App">
